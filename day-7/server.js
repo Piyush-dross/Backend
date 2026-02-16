1b7530dce0aeecc3a5ffd321666a68b69d0ab47e
Piyush-dross <kumarsinghpiyush62@gmail.com>
2026-02-16 12:06:24 +0530

    Day-7

--- day-7/server.js
+++ day-7/server.js
@@ -1,7 +1,11 @@
 //cluster=storage+processor
 //cluster can hold more than one database
+const mongoose=require("mongoose")
 
-const app=("./src/app")
+require("dotenv").config()
+const connecttodatabase=require("./src/config/database")
+const app=require("./src/app")
+connecttodatabase()
 app.listen(3000,()=>{
-    console.log("hello server start")
+    console.log("server start")
 })
--- day-7/src/app.js
+++ day-7/src/app.js
@@ -1,3 +1,26 @@
 const express=require("express")
 const app=express()
+const noteModel=require("./models/notes.model")
+app.use(express.json())
+app.post("/notes",async(req,res)=>{
+    const{tittle,description,age}=req.body
+    const note= await noteModel.create({
+        tittle,
+        description,
+        age
+    })
+    res.status(201).json({
+        message:"note created successfully",
+        note,
+        age
+    })
+})
+app.get("/notes",async(req,res)=>{
+   const notes= await noteModel.find()
+   res.status(200).json({
+        message:"note created successfully",
+        notes
+     
+    })
+})
 module.exports=app
